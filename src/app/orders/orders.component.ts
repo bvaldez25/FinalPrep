0233bb59d89f0ac7c0ab4dd2262d27f6bbb0888d
Your Name <bvaldez25@mail.fresnostate.edu>
2021-12-11 13:47:29 -0800

    finish exam prep

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FlexModalService } from '../shared-components/flex-modal/flex-modal.service';
 import { Http } from '@angular/http';
+import { e } from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-orders',
@@ -12,7 +13,9 @@ import { Http } from '@angular/http';
 export class OrdersComponent implements OnInit {
 
   orders:Array<any> = [];
-
+  name = '';
+  errorMessage = '';
+  confirmMessage = '';
   constructor(
     private router: Router,
     private flexModal: FlexModalService,
@@ -22,21 +25,118 @@ export class OrdersComponent implements OnInit {
   }
 
   async ngOnInit() {
-    
+    this.loadDefaultOrders();
   }
 
-  // prepare result, splice last name, first name
 
-  // Calculate total and perform input validation
-  
-  // display the order form with orders from orders.json
+  calculate(){
+    const total = this.orders.reduce((inc, item, i, arr) =>{
+      inc += item.price * item.quantity;
+      return inc;
+    }, 0);
+    const taxAmount = total * .1;
 
-  // Clear the orders form
+    const subTotal = total - taxAmount;
+    console.log('from calculate() total: ', total, 'taxAmount', taxAmount, 'subtTotal', subTotal);
+    return {
+      total: total,
+      taxAmount: taxAmount,
+      subTotal: subTotal,
+    };
+  }
 
-  // Add items 'Hot Dog', 'Hamberger' and 'Pizza' to list when corresponding button is clicked
+  submit() {
+    const commaIndex = this.name.indexOf(', ');
+    let error = false;
 
-  // delete line item (order) when delete button is click
+    if (this.name === '') {
+      console.log('Name must not be empty!');
+      this.errorMessage= 'Name must not be empty!';
+      error= true;
+    } else if(commaIndex === -1){
+      this.errorMessage= 'Name must have a comma!';
+      error = true;
+    }
 
-  // read in the orders.json file and populate the list table with the initial orders (3)
+    if (!error) {
+      const firstName = this.name.slice(commaIndex + 1, this.name.length);
+      const lastName = this.name.slice(0, commaIndex);
+      const fullName = firstName + '' + lastName;
+      const calculation = this.calculate();
+      this.confirmMessage = 'Thank you for your order ${fullName}. Your subtotal is: ${calculation.subTotal}. Your tax amount is ${calculation.taxAmount}. Your grand total is ${calculation.total}.',
+      this.flexModal.openDialog('confirm-modal');
+    }else{
+      this.flexModal.openDialog('error-modal');
+    }
+    console.log('this.name', this.name, 'commaIndex', commaIndex, 'firstName', firstName, 'lastName', lastName);
+
+  }
+
+  loadDefaultOrders() {
+    this.orders = [{
+      "pid": "1",
+      "image":"assets/sm_hotdog.jpeg",
+      "description": "Hot Dog",
+      "price": 5.00,
+      "quantity": 2
+    }, {
+      "pid": "2",
+      "image":"assets/sm_hamberger.jpeg",
+      "description": "Hamburger",
+      "price": 6.00,
+      "quantity": 1
+    }, {
+      "pid": "3",
+      "image":"assets/sm_pizza.jpeg",
+      "description": "Large Pizza",
+      "price": 12.00,
+      "quantity": 2
+    }]
+
+  }
+
+  delete(index: number) {
+    this.orders.splice(index, 1);
+  }
+
+  addItem(item: string) {
+    switch(item) {
+      case 'hot dog':
+        this.orders.unshift({
+        "pid": "1",
+        "image":"assets/sm_hotdog.jpeg",
+        "description": "Hot Dog",
+        "price": 5.00,
+        "quantity": 0
+        });
+      break;
+      case 'hamburger':
+        this.orders.unshift({
+          "pid": "2",
+          "image":"assets/sm_hamberger.jpeg",
+          "description": "Hamburger",
+          "price": 6.00,
+          "quantity": 0
+        })
+      break;
+      case 'pizza':
+        this.orders.unshift({
+          "pid": "3",
+          "image":"assets/sm_pizza.jpeg",
+          "description": "Large Pizza",
+          "price": 12.00,
+          "quantity": 0
+        })
+      break;
+    }
+  }
 
 }
+function firstName(arg0: string, name: string, arg2: string, commaIndex: number, arg4: string, firstName: any, arg6: string, lastName: any) {
+  throw new Error('Function not implemented.');
+}
+
+function lastName(arg0: string, name: string, arg2: string, commaIndex: number, arg4: string, firstName: (arg0: string, name: string, arg2: string, commaIndex: number, arg4: string, firstName: any, arg6: string, lastName: any) => void, arg6: string, lastName: any) {
+  throw new Error('Function not implemented.');
+}
+
